76b3e0ba9e3e186a10a3d53b149167e81150e595
wanghe <wanghe@virtuosgames.com>
2014-09-22 14:36:01 +0800

    add customer loader image

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ cc.game.onStart = function(){
     cc.view.setDesignResolutionSize(1024, 723, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(true);
     //load resources
+//    cc._loaderImage="res/assets/assets01/symbolMask.png"//set customer loader image or over write the loaderScene
     cc.LoaderScene.preload(g_resources, function () {
 //        cc.director.runScene(new HelloWorldScene());
         cc.director.runScene(new GameScene());
